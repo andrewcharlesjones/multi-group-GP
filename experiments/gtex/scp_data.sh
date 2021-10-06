14c4d2ef3876d70d606d692d5992db7d92844305
Andy Jones <ajones788@gmail.com>
2021-10-06 12:47:09 -0400

    add experiments

--- experiments/gtex/scp_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-scp ../../data/gtex/*.csv aj13@della.princeton.edu:/scratch/gpfs/aj13/multi-group-GP/data/gtex
-
-echo "Done."
--- experiments/gtex/scp_results.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-scp aj13@della.princeton.edu:/scratch/gpfs/aj13/multi-group-GP/experiments/gtex/*.csv .
-
-echo "Done."
