92efb0493c513a1046ae6c2a957203cb7707524e
Andy Jones <ajones788@gmail.com>
2021-10-10 15:15:26 -0400

    slight name change

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mggp",
-    version="0.3",
+    name="multi-group-GP",
+    version="0.1",
     description="Multi-group Gaussian processes",
     url="https://github.com/andrewcharlesjones/multi-group-GP",
-    download_url="https://github.com/andrewcharlesjones/pcpca/archive/refs/tags/v_03.tar.gz",
+    download_url="https://github.com/andrewcharlesjones/multi-group-GP/archive/refs/tags/v01.tar.gz",
     author="Andy Jones",
     author_email="ajones788@gmail.com",
     packages=find_packages(),
