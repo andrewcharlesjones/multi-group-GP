5ab0bef75a0d7678b9c4a86a246681727ebdc4c1
Andy Jones <ajones788@gmail.com>
2021-10-18 15:05:08 -0400

    new version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="multigroupGP",
-    version="1.0",
+    version="1.1",
     description="Multi-group Gaussian processes",
     url="https://github.com/andrewcharlesjones/multi-group-GP",
-    download_url="https://github.com/andrewcharlesjones/multi-group-GP/archive/refs/tags/v_1.tar.gz",
+    download_url="https://github.com/andrewcharlesjones/multi-group-GP/archive/refs/tags/v1.1.tar.gz",
     author="Andy Jones",
     author_email="ajones788@gmail.com",
     packages=find_packages(),
